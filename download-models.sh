1a050caa7e6895ce09fb7fc076f59b866cffa35c
administrator <administrator@GPU-dedicated-server.com>
2025-03-14 07:53:58 -0500

    works at avoiding overwrite of already-downloaded file

--- download-models.sh
+++ download-models.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-wget https://huggingface.co/deepseek-ai/DeepSeek-V3-Base/resolve/main/model-00001-of-000163.safetensors?download=true
+wget -N https://huggingface.co/deepseek-ai/DeepSeek-V3-Base/resolve/main/model-00002-of-000163.safetensors
